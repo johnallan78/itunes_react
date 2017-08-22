95ab8a80889d9119a784ce36d28d397089488b21
John Allan <johnjamesharvey@gmail.com>
2017-08-22 21:12:21 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import SongContainer from './container/SongContainer.jsx'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+             <SongContainer />
+            </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/container/SongContainer.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import Song from '../components/SongList';
+
+
+class SongContainer extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      songs: []
+    };
+  }
+
+  componentWillUpdate(){
+    const url = "https://itunes.apple.com/gb/rss/topsongs/limit=20/json"
+    const request = new XMLHttpRequest();
+    request.open("GET", url);
+    console.log(url)
+    request.addEventListener('load', () => {
+      if(request.status === 200){
+        
+        const jsonString = request.responseText;
+        const data = JSON.parse(jsonString);
+
+        this.setState({
+          songs: data,
+        });
+      }
+    });
+    request.send()
+  }
+
+  render(){
+    return(
+        <div>
+          <Song song={this.state.songs} />
+        </div>
+      );
+  }
+
+
+}
+
+export default SongContainer;
